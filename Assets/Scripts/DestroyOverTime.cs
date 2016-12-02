9b75e25ac147e5d014a1a935b04b0efb874bb85e
Scott Bromander <scott@primeacademy.io>
2016-12-01 21:04:00 -0600

    destory over time script

---
+++ Assets/Scripts/DestroyOverTime.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOverTime : MonoBehaviour {
+	public float lifeTime;
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime -= Time.deltaTime;
+		if (lifeTime < 0) {
+			Destroy (gameObject);
+		}
+	}
+}
