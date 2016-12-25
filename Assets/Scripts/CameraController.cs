f47cef4e574b2234a3ec785bf7b6dc7f0637c407
Scott Bromander <scott@primeacademy.io>
2016-12-25 15:42:35 -0600

    camera lerp

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,16 @@ public class CameraController : MonoBehaviour {
     
 	public Transform target;
 
+	public float cameraSpeed;
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(target.position.x,target.position.y, transform.position.z);
+//		transform.position = new Vector3(target.position.x,target.position.y, transform.position.z);
+
+		transform.position = Vector3.Lerp (transform.position, new Vector3 (target.position.x, target.position.y, transform.position.z), cameraSpeed * Time.deltaTime);
 	}
 }
