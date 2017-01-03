5ef6cef02ecca46d5bd7629bebb523e867171403
Scott Bromander <scott@primeacademy.io>
2017-01-02 20:17:50 -0600

    screen shake conditional

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -25,5 +25,15 @@ public class CameraController : MonoBehaviour {
 //		transform.position = new Vector3(target.position.x,target.position.y, transform.position.z);
 
 		transform.position = Vector3.Lerp (transform.position, new Vector3 (target.position.x + (moveAhead * target.localScale.x), target.position.y, transform.position.z), cameraSpeed * Time.deltaTime);
+	
+		if (screenShakeAmount > 0) {
+			screenShakeActive = new Vector3 (Random.Range (-screenShakeAmount, screenShakeAmount),
+				Random.Range (-screenShakeAmount, screenShakeAmount), 0f);
+			screenShakeAmount -= Time.deltaTime * screenShakeDecay;
+		} else {
+			screenShakeActive = Vector3.zero;
+		}
+
+		transform.position += screenShakeActive;
 	}
 }
