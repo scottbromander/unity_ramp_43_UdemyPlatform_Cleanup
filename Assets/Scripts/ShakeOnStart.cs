ab6cf11c4418031de899d63e3f02f3e0cc72f9f3
Scott Bromander <scott@primeacademy.io>
2017-01-03 20:36:36 -0600

    configure shake on start

--- Assets/Scripts/ShakeOnStart.cs
+++ Assets/Scripts/ShakeOnStart.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class ShakeOnStart : MonoBehaviour {
 
+	public float screenShakeAmount;
+	private CameraController cameraController;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		cameraController = FindObjectOfType<CameraController> ();
+		cameraController.ScreenShake (screenShakeAmount);
 	}
 }
